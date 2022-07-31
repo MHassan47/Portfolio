dd731959ba51316d58a81d8c2cbf1c00298f8403
Mohamed <mhassankd35@gmail.com>
2022-07-31 16:30:27 -0500

    adds more key values

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -2,25 +2,43 @@ const portfolio = [
   {
     id: "1",
     title: "Produc_Div",
+    description: "Collaborative productivity management app",
+    img: "../../Docs/dashboard.PNG",
+    keyword: ["React", "PostgreSQL", "Express.js", "WebSocket"],
   },
   {
     id: "2",
     title: "Scheduler",
+    description: "Real-Time interview scheduler",
+    img: "../../Docs/mainpage.png",
+    keyword: ["React", "PostgreSQL", "Express.js"],
   },
   {
     id: "3",
     title: "Jungle",
+    description: "E-commerce plant store that supports Stripe payment",
+    img: "../../Docs/jungle.png",
+    keyword: ["Ruby", "Rails", "PostgreSQL"],
   },
   {
     id: "4",
     title: "Passwordle",
+    description: "Wordle inspired game",
+    img: "../../Docs/passwordle_loser.png",
+    keyword: ["React"],
   },
   {
     id: "5",
     title: "Map it",
+    description: "Collaborative, interactive map",
+    img: "../../Docs/map_it.PNG",
+    keyword: ["JQuery", "Ajax", "Express.js", "EJS", "PostgreSQL"],
   },
   {
     id: "6",
     title: "React-Weather",
+    description: "Weather App ",
+    img: "../../Docs/san_francisco_weather.PNG",
+    keyword: ["React"],
   },
 ];
