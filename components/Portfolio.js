083f4fb42bf46c80a3ddf142db1f025b5a3ac8a6
Mohamed <mhassankd35@gmail.com>
2022-07-31 01:26:18 -0500

    creates portfolio data file

---
+++ components/Portfolio.js
@@ -0,0 +1,26 @@
+const portfolio = [
+  {
+    id: "1",
+    title: "Produc_Div",
+  },
+  {
+    id: "2",
+    title: "Scheduler",
+  },
+  {
+    id: "3",
+    title: "Jungle",
+  },
+  {
+    id: "4",
+    title: "Passwordle",
+  },
+  {
+    id: "5",
+    title: "Map it",
+  },
+  {
+    id: "6",
+    title: "React-Weather",
+  },
+];
