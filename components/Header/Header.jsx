abf82017550b3bc24a34125e29aac23b30251f7a
Mohamed <mhassankd35@gmail.com>
2022-07-31 02:18:44 -0500

    creates nav links in header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Container } from "reactstrap";
+import classes from "./header.module.css";
+import Link from "next/link";
 
+const NAV_LINK = [
+  {
+    path: "/",
+    display: "Home",
+  },
+  {
+    path: "#about",
+    display: "About",
+  },
+  {
+    path: "#portfolio",
+    display: "Portfolio",
+  },
+  {
+    path: "#contact",
+    display: "Contact",
+  },
+];
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={`${classes.header}`}>
+      <Container>
+        <div className={`${classes.nav_wrapper}`}>
+          <h1>
+            <span>M</span>ohamed
+          </h1>
+        </div>
+        <div className="{`${classes.navigation}`}">
+          <div className="{`${classes.nav_menu}`}">
+            {NAV_LINK.map((item, index) => (
+              <Link href={item.path} key={index}>
+                {item.display}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
