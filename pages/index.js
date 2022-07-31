9056f697c6179b25b134a8d8364152c3c78ca4c4
Mohamed <mhassankd35@gmail.com>
2022-07-31 15:33:29 -0500

    imports About component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import About from "../components/UI/About";
 import Hero from "../components/UI/Hero";
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
