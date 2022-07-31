74ad7f55d92a6da65865d3b4616d001f7980e731
Mohamed <mhassankd35@gmail.com>
2022-07-31 15:38:24 -0500

    creates portfolio component

---
+++ components/UI/Portfolio.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Link from "next/link";
+
+function Portfolio() {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col lg="6" md="6"></Col>
+          <h2>Portfolio</h2>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Portfolio;
