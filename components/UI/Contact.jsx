ffee4e3097d885eefe8ee5badb1561cfe83f66c0
Mohamed <mhassankd35@gmail.com>
2022-07-31 17:43:26 -0500

    creates Contact component

---
+++ components/UI/Contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Link from "next/link";
+function Contact() {
+  return (
+    <section id="contact">
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <h5>Contact</h5>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default Contact;
