6dd6b63dd32d902b78e69fbb737de5740f6e199c
Mohamed <mhassankd35@gmail.com>
2022-07-31 13:44:16 -0500

    renders hello from props

--- components/UI/SectionSubtitle.jsx
+++ components/UI/SectionSubtitle.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "../../styles/subtitle.module.css";
 function SectionSubtitle(props) {
-  return <div>{props.subtitle}</div>;
+  return <div className={`${classes.section_subtitle}`}>{props.subtitle}</div>;
 }
 
 export default SectionSubtitle;
