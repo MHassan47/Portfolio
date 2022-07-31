b947147bfb13de0988fa67b22dac028cb35df8bb
Mohamed <mhassankd35@gmail.com>
2022-07-31 12:53:01 -0500

    creates subtitle component in Hero

---
+++ components/UI/SectionSubtitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "../../styles/subtitle.module.css";
+function SectionSubtitle(props) {
+  return <div>{props.subtitle}</div>;
+}
+
+export default SectionSubtitle;
