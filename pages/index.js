aac508e077b35d703c614d99ad76d06ff3debe40
Mohamed <mhassankd35@gmail.com>
2022-07-31 15:38:06 -0500

    imports portfolio component

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import About from "../components/UI/About";
 import Hero from "../components/UI/Hero";
+import Portfolio from "../components/UI/Portfolio";
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Portfolio />
     </>
   );
 }
