12687624e0e13a65309719d785a4f803f57a5e67
Mohamed <mhassankd35@gmail.com>
2022-08-01 04:17:00 -0500

    imports Skills component

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ import About from "../components/UI/About";
 import Contact from "../components/UI/Contact";
 import Hero from "../components/UI/Hero";
 import Portfolio from "../components/UI/Portfolio";
+import Skills from "../components/UI/Skills";
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Skills />
       <Portfolio />
       <Contact />
     </>
