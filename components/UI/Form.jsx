56e516b4eb30e7191b6141e3eb4251621afa8ba8
Mohamed <mhassankd35@gmail.com>
2022-07-31 19:44:20 -0500

    refactors to include local state

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../../styles/form.module.css";
 function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <form className={`${classes.form}`}>
       <div className={`${classes.form_group}`}>
-        <input type="text" placeholder="Your Name" required />
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
       </div>
       <div className={`${classes.form_group}`}>
-        <input type="text" placeholder="Email Address" required />
+        <input
+          type="text"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
       </div>
       <div className={`${classes.form_group}`}>
-        <textarea type="text" placeholder="Message" required />
+        <textarea
+          type="text"
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        />
       </div>
       <button className="primary_btn" type="submit">
         Send
