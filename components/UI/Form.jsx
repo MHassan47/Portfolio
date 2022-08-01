ce88d1b3e96e2abb4852d281aa6d5ad8b1ca2eb8
Mohamed <mhassankd35@gmail.com>
2022-07-31 19:29:19 -0500

    creates form component

---
+++ components/UI/Form.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "../../styles/form.module.css";
+function Form() {
+  return (
+    <form className={`${classes.form}`}>
+      <div className={`${classes.form_group}`}>
+        <input type="text" placeholder="Your Name" required />
+      </div>
+      <div className={`${classes.form_group}`}>
+        <input type="text" placeholder="Email Address" required />
+      </div>
+      <div className={`${classes.form_group}`}>
+        <textarea type="text" placeholder="Message" required />
+      </div>
+      <button className="primary_btn" type="submit">
+        Send
+      </button>
+    </form>
+  );
+}
+
+export default Form;
