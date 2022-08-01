bac8e295808820736ef325f887e31ee1ed10ceee
Mohamed <mhassankd35@gmail.com>
2022-07-31 20:04:44 -0500

    implements emailjs api

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import classes from "../../styles/form.module.css";
+import emailjs from "@emailjs/browser";
 function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_xboiwzp",
+        "template_xxc79i8",
+        form.current,
+        "z8DjKfqOaEAh14jln"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
-    <form className={`${classes.form}`}>
+    <form ref={form} className={`${classes.form}`} onSubmit={sendEmail}>
       <div className={`${classes.form_group}`}>
-        <input
-          type="text"
-          placeholder="Your Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
+        <input type="text" placeholder="Your Name" name="name" required />
       </div>
       <div className={`${classes.form_group}`}>
-        <input
-          type="text"
-          placeholder="Email Address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+        <input type="text" placeholder="Email Address" name="email" required />
       </div>
       <div className={`${classes.form_group}`}>
-        <textarea
-          type="text"
-          placeholder="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        />
+        <textarea type="text" placeholder="Message" name="message" required />
       </div>
       <button className="primary_btn" type="submit">
         Send
