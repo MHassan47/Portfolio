e30b60d7296b16daef6ae5e8d7ebb44517decfc2
Mohamed <mhassankd35@gmail.com>
2022-08-02 00:06:16 -0500

    imports all logo images

---
+++ components/SkillsData.js
@@ -0,0 +1,17 @@
+const SkillsData = [
+  "/images/javascript.PNG",
+  "/images/react.png",
+  "/images/postgresql.jpg",
+  "/images/rails.png",
+  "/images/ruby.PNG",
+  "/images/node.PNG",
+  "/images/MongoDB.PNG",
+  "/images/html.PNG",
+  "/images/css.png",
+  "/images/bootstrap.jpg",
+  "/images/tailwind.png",
+  "/images/typescript.PNG",
+  "/images/jquery.PNG",
+];
+
+export default SkillsData;
