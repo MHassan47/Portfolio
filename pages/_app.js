6fb6bb8797503ca4c5b4b7e25b6ff5894065c5d5
Mohamed <mhassankd35@gmail.com>
2022-08-02 23:56:38 -0500

    wraps app around animation method

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import Layout from "../components/Layout/Layout";
+import { AnimatePresence } from "framer-motion";
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AnimatePresence>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimatePresence>
   );
 }
 
