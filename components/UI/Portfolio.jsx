3d4185037a276fa998674cd9bf4b87d4e1539d79
Mohamed <mhassankd35@gmail.com>
2022-08-02 23:55:53 -0500

    adds animation to portfolio component

--- components/UI/Portfolio.jsx
+++ components/UI/Portfolio.jsx
@@ -1,24 +1,64 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { useScroll } from "../useScroll";
 import PortfolioData from "../PortfolioData";
 import PortfolioItem from "./PortfolioItem";
 function Portfolio() {
-  console.log(PortfolioData);
+  const [element, controls] = useScroll();
+  const container = {
+    show: {
+      transition: {
+        staggerChildren: 0.55,
+      },
+    },
+  };
+
+  const item = {
+    hidden: {
+      opacity: 0,
+      y: 200,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: [0.6, 0.01, -0.05, 0.95],
+        duration: 1.6,
+      },
+    },
+  };
   return (
-    <section id="portfolio">
+    <motion.section
+      key="portfolio"
+      ref={element}
+      id="portfolio"
+      variants={container}
+      initial="hidden"
+      animate="show"
+      transition={{
+        delay: 0.03,
+        type: "tween",
+        duration: 0.6,
+      }}
+    >
       <Container>
         <Row>
           <h1>Portfolio</h1>
 
           {PortfolioData.map((project) => (
             <Col lg="4" md="4" key={project.id} className="mb-5">
-              <PortfolioItem project={project} />
+              <PortfolioItem
+                project={project}
+                variants={item}
+                controls={controls}
+              />
             </Col>
           ))}
         </Row>
       </Container>
-    </section>
+    </motion.section>
   );
 }
 
