6e4ab09e42b27c4e26cd659420bc408561e462e8
Mohamed <mhassankd35@gmail.com>
2022-08-02 23:57:00 -0500

    imports and renders scrollToTop button

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import About from "../components/UI/About";
 import Contact from "../components/UI/Contact";
 import Hero from "../components/UI/Hero";
@@ -8,6 +9,7 @@ import Skills from "../components/UI/Skills";
 export default function Home() {
   return (
     <>
+      <ScrollToTop />
       <Hero />
       <About />
       <Skills />
