40628b72651168786d9d761f2f8b7a139e308a97
Mohamed <mhassankd35@gmail.com>
2023-01-15 12:33:24 -0600

    modifies animations, changes about me content

--- components/UI/About.jsx
+++ components/UI/About.jsx
@@ -6,8 +6,8 @@ function About() {
   return (
     <motion.section
       id="about"
-      initial={{ x: -1000, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
+      initial={{ y: 1000, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
       transition={{ delay: 1.1, duration: 0.8, type: "tween" }}
     >
       <Container>
@@ -15,14 +15,12 @@ function About() {
           <Col lg="12" md="12">
             <h2 className="about_me">About Me</h2>
             <p>
-              After some exposure to interdisciplinary projects blending
-              computer science and life science, I became inspired to pivot from
-              an academic background in Microbiology and Chemistry into the
-              world of tech! I am a life long learner who is looking to utilize
-              valuable experience in research, development and execution of
-              refined projects into the tech space. <br /> Aside from coding, I
-              have several hobbies I am passionate about. I love video games,
-              basketball and finance!
+              A background in Bioinformatics which blends Microbiology and
+              Computer Science piqued my interest in software development.
+              <br /> A life long learner looking to bring valuable experience in
+              research and development of refined projects into my next role.{" "}
+              <br /> Aside from coding, I have several hobbies I am passionate
+              about. I love video games, basketball and finance!
             </p>
             <motion.div
               className=" d-flex align-items-center gap-5"
